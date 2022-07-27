16c80be4d35a9c568688eceee073e883aaf4d2b7
Kaitlin Yen <109048346+kayenpepper@users.noreply.github.com>
2022-07-27 17:28:45 +0000

    Nav buttons add corresponding toppings to the order; total price adds the new prices to the sum and doesn't go over.

--- script.js
+++ script.js
@@ -10,17 +10,26 @@ toppings.forEach((topping) => {
 
 // not querySelectorAll because there is only one checkout button
 const checkoutButton = document.querySelector(".button");
-let total = 0;
 // total selector
 const answerHolder = document.querySelector("#total");
 // add prices for checkout
 checkoutButton.addEventListener("click", (e) => {
+  // add all the toppings under order
+// nesting format like calling css
+let total = 0;
+  const ingredients = document.querySelectorAll(".order .topping");
   ingredients.forEach((ingredient) => {
     total += Number(ingredient.dataset.price);
   });
   answerHolder.innerHTML = `The total cost of your burger is $${total}`;
 })
 
-// add all the toppings under order
-// nesting format like calling css
-const ingredients = document.querySelectorAll(".order .topping");
+
+
+const navButtons = document.querySelectorAll("#nav .topping");
+const orderDiv = document.querySelector(".order");
+navButtons.forEach((navButton) => {
+  navButton.addEventListener("click", () => {
+    orderDiv.innerHTML += `<div class="topping ${navButton.dataset.ingredient}" data-ingredient="${navButton.dataset.ingredient}" data-price="${navButton.dataset.price}">${navButton.dataset.ingredient}</div>`;
+  });
+});
